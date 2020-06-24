e9ade1c53986d1ff96b7fadc601a40f47ec9b665
Bayard75 <bayard.maniraho@gmail.com>
2020-06-24 18:47:24 +0200

    son model ajouté et fonctionne

--- triplodocus/website/models.py
+++ triplodocus/website/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+#  Create your models here.
+#  The only model needed here are songs
+
+
+class Son(models.Model):
+    titre = models.CharField(primary_key=True, max_length=255)
+    titre_image = models.ImageField(default='default.jpg', upload_to='son_pics')
+    poster_image = models.ImageField(default='default.jpg', upload_to='son_poster')
+    resume = models.TextField()
+    realisation = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+    youtube = models.URLField()
+    deezer = models.URLField()
+    spotify = models.URLField()
+
+    def __str__(self):
+        return f'{self.titre} son model.'
+
